14b0f950ea3c869a48e822ec3abcba2b45786d0f
Spencer Rosio <spencerrosio3d@gmail.com>
2020-12-10 20:40:19 -0600

    set up wait list and adjusted get urls

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let app = express();
 let PORT = process.env.PORT || 3200;
 
 let reservationsList = [];
+let waitList = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,15 +17,19 @@ app.get("/", function (request, response) {
     response.sendFile(path.join(__dirname, "homepage.html"));
 });
 
-app.get("/make-reservation", function (request, response) {
+app.get("/reserve", function (request, response) {
     response.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/view-reservations", function (request, response) {
+app.get("/tables", function (request, response) {
     response.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/api/reservations", function (request, response) {
+app.get("/api/waitlist", function (request, response) {
+    return response.json(waitList);
+});
+
+app.get("/api/tables", function (request, response) {
     return response.json(reservationsList);
 });
 
@@ -32,9 +37,15 @@ app.get("/api/reservations", function (request, response) {
 
 app.post("/api/reservation", function (request, response) {
     let newReservation = request.body;
-    console.log(newReservation);
-    reservationsList.push(newReservation);
-    response.json(newReservation);
+    if(reservationsList.length >4){
+        waitList.push(newReservation);
+        console.log(newReservation);
+        return response.json(false);
+    } else {
+        reservationsList.push(newReservation);
+        console.log(newReservation);
+        return response.json(true);
+    }
 });
 
 app.listen(PORT, function () {
